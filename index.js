1d75fbd46656af35ec206a4b282bd68366b8f034
Ankit Singh <aksingh11411@gmail.com>
2023-07-06 13:42:31 +0530

    style changes

--- index.js
+++ index.js
@@ -12,25 +12,20 @@ let totalMbSpeed = 0;
 let numTests = 1;
 let testCompleted = 0;
 
-// Get random image from unsplash.com
 let imageApi = 'https://source.unsplash.com/random?topic=nature';
 
-// When image loads
 image.onload = async function () {
   endTime = new Date().getTime();
 
-  // Get image size
   await fetch(imageApi).then((response) => {
     imageSize = response.headers.get('content-length');
     calculateSpeed();
   });
 };
 
-// Function to calculate speed
 function calculateSpeed() {
-  // Time taken in seconds
   let timeDuration = (endTime - startTime) / 1000;
-  // Total bits
+
   let loadedBits = imageSize * 8;
   let speedInBts = loadedBits / timeDuration;
   let speedInKbs = speedInBts / 1024;
@@ -42,32 +37,27 @@ function calculateSpeed() {
 
   testCompleted++;
 
-  // If all tests completed (we get 5 image then calculate average)
   if (testCompleted === numTests) {
     let averageSpeedInBps = (totalBitSpeed / numTests).toFixed(2);
     let averageSpeedInKbps = (totalKbSpeed / numTests).toFixed(2);
     let averageSpeedInMbps = (totalMbSpeed / numTests).toFixed(2);
 
-    // Display average speeds
     bitSpeed.innerHTML += `${averageSpeedInBps}`;
     kbSpeed.innerHTML += `${averageSpeedInKbps}`;
     mbSpeed.innerHTML += `${averageSpeedInMbps}`;
     info.innerHTML = 'Test Completed!';
   } else {
-    // Run the next test
     startTime = new Date().getTime();
     image.src = imageApi;
   }
 }
 
-// Initial function to start tests
 const init = async () => {
   info.innerHTML = 'Testing...';
   startTime = new Date().getTime();
   image.src = imageApi;
 };
 
-// Run tests when window loads
 window.onload = () => {
   for (let i = 0; i < numTests; i++) {
     init();
